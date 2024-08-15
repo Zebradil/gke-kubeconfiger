2ba07d74861ad96d6decd1da4e52e689336fb023
German Lashevich <german.lashevich@gmail.com>
2024-08-15 19:42:40 +0200

    Fix type

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pname = "gke-kubeconfiger";
         src = ./.;
         vendorHash = "sha256-BJ5sv5zV50xvlfqaeZcmXl/jEZ9zAdrregSTY+3LSYQ=";
-        version = "${baseVersion}-${self.shortRev or self.dirtyShortRev or self.lastModified or "unknown"}";
+        version = "${baseVersion}-${self.shortRev or self.dirtyShortRev or toString self.lastModified or "unknown"}";
         meta = {
           description = "TBD";
         };
